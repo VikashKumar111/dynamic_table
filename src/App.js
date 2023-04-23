d11b6abf1c14d260269425f180957769622d53ce
Vikash Kumar <vikasthakur9310@gmail.com>
2023-04-23 17:32:08 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
 
-import './App.css';
+const API = "https://api.postman.com/collections/24582109-37d97559-22b0-42e0-b592-7fd8b90b8e01?access_key=PMAT-01GXAEX88FNRZN45AWACQ2V20F";
+
 
 const App = () => {
+    
+    const fetchUsers = async(url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+        }catch (e) {
+            console.error(e)
+        }
+    }
 
- 
+    useEffect(() => {fetchUsers(API)}, [])
+    return <>
+        <h1>React table</h1>
+    </>
+    
+     
 };
 export default App;
